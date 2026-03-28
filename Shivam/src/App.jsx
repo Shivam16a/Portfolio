ece4b0349b4ce32d55318110d0591fe39aebb527
Shivam16a <shivam123hjp@gmail.com>
2026-03-29 00:05:38 +0530

    Creating component and pages

--- Shivam/src/App.jsx
+++ Shivam/src/App.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   )
