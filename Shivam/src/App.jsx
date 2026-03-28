806627fd50e2c661680d37c480f7d69484a7f2a8
Shivam16a <shivam123hjp@gmail.com>
2026-03-28 23:26:41 +0530

    initilize Project

--- Shivam/src/App.jsx
+++ Shivam/src/App.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
