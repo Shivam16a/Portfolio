e794631a443e2fbbedac3d5c371e03d27511e0e7
Shivam16a <shivam123hjp@gmail.com>
2026-04-01 23:13:50 +0530

    connect to database

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,18 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
+const dotenv = require("dotenv");
+dotenv.config();
+const connectdb = require("./config/db.js");
 
+
+
+
+
+const PORT = process.env.PORT || 5500;
+
+connectdb().then(() => {
+    app.listen(PORT,()=>{
+        console.log(`Server is running on port:${PORT}`);
+    })
+})
